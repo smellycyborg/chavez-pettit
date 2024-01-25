78422aff29b606df963ba717bb8ea80f012a6737
chavez <chavez.swagger@gmail.com>
2024-01-25 15:17:01 -0600

    update

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom'
+import '@testing-library/jest-dom/extend-expect'
 import App from './App';
 
 test('renders learn react link', () => {
