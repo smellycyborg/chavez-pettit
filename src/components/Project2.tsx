72d4a1cc198d6149ec6af11dab78b745d4b2cf85
chavez <chavez.swagger@gmail.com>
2024-01-25 16:26:32 -0600

    update project 2

--- src/components/Project2.tsx
+++ src/components/Project2.tsx
@@ -26,23 +26,23 @@ export const Project2: React.FC<Props> = ( { name, link1 } ) => {
             <Player 
             height='100%'
             width='100%'
-            className='react--player' url="https://youtu.be/uGErkQ34kXk" />
+            className='react--player' url="https://youtu.be/uLj4l2Um1Hc?si=o33kkR8xBb5Je9VA" />
             </div>
             <ul data-type='circle'>
-                <li>a video game hosted on roblox.</li>
+                <li>a gunz the duel remake</li>
                 <li>
-                    players can purchase and equip pets, 
-                    grow their character through clicking, 
-                    fight in arena, and teleport to different worlds.
+                    players can perform two combo moves.  
+                    players can pick up health and armor.  
+                    players can run on walls and dash.
                 </li>
             </ul>
             <small><p className='center--text'>tech:  lua</p></small>
-            <div className='icon--container'>
+            {/* <div className='icon--container'>
             
             <Icon className='icon' icon="uit:github-alt" height="22" 
                 onClick={(e: React.MouseEvent<Element, MouseEvent>) => {
                     click1(e, link1, 'clicked git')}} />
-            </div>
+            </div> */}
         </div>
     )
 }
